cd875a6a10605fcca8469255c917ce6854d56929
aMigod666(KyleJRoux) <decimalwarrior@gmail.com>
2013-01-09 17:11:54 +0000

    Added the Final version of PDplayerClass.py, i know its not the first time ive said that, but this time really.

--- PDplayerClass.py
+++ PDplayerClass.py
@@ -1,37 +1,129 @@
-# PDplayerClass.py
-# -*- coding: utf-8 *-*
+# PDplayerClass.py // Player class file for pyDice10k
 # by: aMigod666(KyleJRoux)
-#import PDmenu
-#import time
-
-debug = True
+import random
+import PDrollClass
+import PDmenu
 
 class Player(object):
-    _playerCount = 0
-    _playerList = [None]
-    _playerScores = dict()
+    '''Players interact with the game while keeping track of individual 
+    Users data(per instance), and keeps track of the current player as
+    well as the number of players(at the class level)
+	
+    class attrs  - _playerList, _playerCounter, _firstNumberList
 
-    def  __init__(self, name=''):
-        Player._playerCount += 1
-        if not (name == ''):
-            self._name = name
-        else:
-            self._name = 'Player #{0}'.format(Player._playerCount)
-        self.playerNumber = Player._playerCount
+    instance attrs - _name, _playerNumber, _firstNum, _diceToRoll,
+    _diceToHold, _isRolling, _gameScore, _rollScore
+
+    class methods - get_player_count, {get,set}_first_player, add_a_player
+
+    instance methods - roll_dice(dieNum), {get,set}_{roll,game}_score,
+    print_roll, {get,set}_held_dice'''
+    #CLASS DATA
+    _playerList = list()
+    _playerCounter = 0
+    _firstNumberList = dict()
+    # CONSTRUCTOR
+    def __init__(self):
+        Player._playerCounter += 1
+        self._playerNumber = Player._playerCounter
+        self._name = raw_input('\n\n\nPlease enter name of player {0}: '.format(self._playerNumber))
+        print 'Welcome {0}'.format(self._name)
         Player._playerList.append(self._name)
-        Player._playerScores[self._name] = 0
-
-        if (debug):
-            print '{0} has joined the game and is player #{1}'.format(self._name, Player._playerCount)
+        self._firstNum = random.choice(range(1, 101))
+        Player._firstNumberList[self._name] = self._firstNum
+        self._diceHolding = list()
+        self._diceRolling = list()
+        self._isRolling = False
+        self._rollScore = 0
+        self._gameScore = 0
+        self._tempScore = 0
 
     def __str__(self):
-            return str(self._name)
+        return 'Player #{0}: {1}'.format(self._playerNumber, self._name)	
+
+    
+    #CLASS METHODS
+    @staticmethod
+    def get_player_count():
+        return Player._playerCounter
+    @staticmethod
+    def print_player_count():
+        print Player.get_player_count()
+    @staticmethod
+    def get_first_player():
+        #code to sort Player._firstNumberList and find biggest
+        pass
+    @staticmethod
+    def set_first_player():
+        # code to set isPlaying to True for highest _firstNum
+		pass
+    @staticmethod
+    def add_player(objRef):
+        # code to add a player with a method
+        objRef = Player()
+
+    #INSTANCE METHODS
+    def roll_dice(self, dieNum=6):
+        self.diceRolling = self.roll(dieNum)
+
+
+    def print_roll(self):
+        self.roll_dice()
+        for die in range(len(self.diceRolling)):
+            print self.diceRolling[die],
+            print ' ',
+	
+    def roll(self, num=6):
+        tmpRoll = list()	
+        for i in range(num):
+            tmpRoll.append(random.choice(range(1, 7)))
+        return tmpRoll			
+
+    def set_held_dice(self):
+        pass	
+
+    def get_held_dice(self):
+        pass	
+
+    def set_roll_score(self):
+        pass	
+
+    def get_roll_score(self):
+        pass	
+
+    def set_game_score(self):
+        pass	
+
+    def get_game_score(self):
+        pass	
+
+		
 
 def main():
-    kyle = Player('Kyle')
+    #P1 = 'Kyle'
+    #Player.add_player(P1)
+    #P1.print_roll()
+    x = Player()
+    print 'roll one'
 
+    x.print_roll()
+    
+    print 'roll two'
+
+    x.print_roll()
+
+    print 'roll three'
+    
+    y = x.roll()	
+    print y
+    print type(y)
+
+    print x
 
 
 
 if __name__ == "__main__":
     main()
+
+
+ 
--- PDrollerClass.py
+++ PDrollerClass.py
@@ -2,19 +2,41 @@
 import random
 
 class Dice(object):
+    Value = {'One' : 100, 'Two' : 20, 'Three' : 30, 'Four' : 40, 'Five' : 50, 'Six' : 60}
+    Face = {1 : 'One', 2 : 'Two', 3 : 'Three', 4 : 'Four', 5 : 'Five', 6 : 'Six'}
+    counts = {'One' : 0, 'Two' : 0, 'Three' : 0, 'Four' : 0, 'Five' : 0, 'Six' : 0}
+
     def __init__(self, rolled=True):
-	    self.faces = range(1, 7)
-	    self.rolled = rolled
+            self.faces = range(1, 7)
+            self.rolled = rolled
 
     def __str__(self):
         if (self.rolled):
-		    return str(random.choice(self.faces))
+                    return str(self.current_value())
         else:
             return str(self.faces)
+
+    def current_value(self):
+        self.currentValue = random.choice(self.faces)
+        Dice.counts[Dice.Face[self.currentValue]] += 1
+
     def make_roll(self):
-        return random.choice(self.faces)
+        return Dice.Face[random.choice(self.faces)]
+
     def roll(self):
-        print self.make_roll()
+        return self.make_roll(),
+
+    def get_face(self):
+        return self.face
+
+    def print_face(self):
+        print self.get_face()
+
+    def get_value(self):
+        return self.value
+
+    def print_value(self):
+        print self.get_value()
 
 
 class Roller(object):
@@ -23,18 +45,40 @@ class Roller(object):
         self.dies = list()
         for i in range(self.dieNum):
             self.dies.append(Dice())
+        self.numCounts = dict()
+
     def roll(self):
+        self.dieList = []
         for i in self.dies:
-            print i, 
+            self.dieList.append(i.roll())
+        for i in range(len(self.dieList)):
+            print str(self.dieList[i]) ,
             print '',
+
+    def get_face_counts(self):
+        #for i in range(1, 7):
+        #    self.numCounts[Dice.Face[i]] = Dice.counts[Dice.Face[i]]
+        pass
+
+    def print_face_counts(self):
+        #self.get_face_counts()
+        #for i in self.numCounts:
+        #    print '{0} {1}'.format(i, self.numCSounts[i])
+        print Dice.counts
+
+
 
 if __name__ == "__main__":
     aRoll = Roller()
     for i in range(1, 7):
-        print 'Roll %d - ' % (i),		
-        aRoll.roll()	
+        print 'Roll %d - ' % (i)
+        aRoll.roll(),
         print
+        aRoll.print_face_counts()
         #print
 
-				
-	    
+    print 'now another roll'
+    aRoll.roll()
+    aRoll.dies[1].current_value()
+    print aRoll.dies[1].currentValue
+
